1b7427493833c883518031287444aa1917634938
David-McNiven <davidmcniven@bell.net>
2016-10-04 16:58:24 -0400

    added routing and navbar

--- portfolio/routes/index.js
+++ portfolio/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'David McNiven' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'My Projects' });
+});
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'My Services' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
